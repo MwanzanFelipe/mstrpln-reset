c284d1bc296facd21fce46786f8b14439e25e560
Ed Rogers <edmund.rogers@gmail.com>
2020-05-16 20:48:05 -0700

    Added view and template for detailed postit as well as CSS

--- datum/urls.py
+++ datum/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.postit_list, name='postit_list'),
+    path('postit/<int:pk>/', views.postit_detail, name='postit_detail'),
 ]
--- datum/views.py
+++ datum/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import PostIt
 
 def postit_list(request):
     postits = PostIt.objects.order_by('created_date')
     return render(request, 'datum/postit_list.html', {'postits': postits})
+
+def postit_detail(request, pk):
+    postit = get_object_or_404(PostIt, pk=pk)
+    return render(request, 'datum/postit_detail.html', {'postit': postit})
