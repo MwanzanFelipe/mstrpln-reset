b8434a5956ea6ee8b88fe77d36c90ee4d16b4c28
Ed Rogers <edmund.rogers@gmail.com>
2020-05-16 21:14:33 -0700

    Added views to create/edit postits inside the site.

--- datum/urls.py
+++ datum/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.postit_list, name='postit_list'),
     path('postit/<int:pk>/', views.postit_detail, name='postit_detail'),
+    path('postit/new/', views.postit_new, name='postit_new'),
+    path('postit/<int:pk>/edit/', views.postit_edit, name='postit_edit'),
 ]
--- datum/views.py
+++ datum/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import PostIt
+from .forms import PostItForm
 
 def postit_list(request):
     postits = PostIt.objects.order_by('created_date')
@@ -9,3 +10,28 @@ def postit_list(request):
 def postit_detail(request, pk):
     postit = get_object_or_404(PostIt, pk=pk)
     return render(request, 'datum/postit_detail.html', {'postit': postit})
+
+def postit_new(request):
+    if request.method == "POST":
+        form = PostItForm(request.POST)
+        if form.is_valid():
+            postit = form.save(commit=False)
+            postit.created_date = timezone.now()
+            postit.save()
+            return redirect('postit_detail', pk=postit.pk)
+    else:
+        form = PostItForm()
+    return render(request, 'datum/postit_edit.html', {'form': form})
+
+def postit_edit(request, pk):
+    postit = get_object_or_404(PostIt, pk=pk)
+    if request.method == "POST":
+        form = PostItForm(request.POST, instance=postit)
+        if form.is_valid():
+            postit = form.save(commit=False)
+            postit.created_date = timezone.now()
+            postit.save()
+            return redirect('postit_detail', pk=postit.pk)
+    else:
+        form = PostItForm(instance=postit)
+    return render(request, 'datum/postit_edit.html', {'form': form})
